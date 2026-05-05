ffb5556743a7c6c5e2d72a48cc6d5cadbdc4845f
beeboee <benjamingibsonw@gmail.com>
2026-05-05 02:24:58 -0700

    Add constants

---
+++ custom_components/peanut_gallery/const.py
@@ -0,0 +1,22 @@
+DOMAIN = "peanut_gallery"
+
+DEFAULT_CACHE_SIZE = 3
+DEFAULT_START_DATE = "1950-10-02"
+DEFAULT_CACHE_DIR = "www/peanut_gallery/cache"
+DEFAULT_CURRENT_IMAGE = "www/peanut_gallery/peanuts.jpg"
+DEFAULT_DATE_FILE = "www/peanut_gallery/peanuts_date.txt"
+DEFAULT_QUEUE_FILE = "peanut_gallery_queue.json"
+
+CONF_CACHE_SIZE = "cache_size"
+CONF_START_DATE = "start_date"
+CONF_CACHE_DIR = "cache_dir"
+CONF_CURRENT_IMAGE = "current_image"
+CONF_DATE_FILE = "date_file"
+CONF_QUEUE_FILE = "queue_file"
+
+SERVICE_TODAY = "today"
+SERVICE_RANDOM = "random"
+SERVICE_DATE = "date"
+SERVICE_REFILL = "refill"
+
+SIGNAL_UPDATED = f"{DOMAIN}_updated"
